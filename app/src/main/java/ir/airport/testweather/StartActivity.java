11bf3cad7cfcc9e914b5344c6519281f3acdeae4
Alisalehivalo <55212861+Alisalehivalo@users.noreply.github.com>
2019-11-28 01:40:08 -0800

    Initial commit

---
+++ app/src/main/java/ir/airport/testweather/StartActivity.java
@@ -0,0 +1,24 @@
+package ir.airport.testweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 4000);
+    }
+}
